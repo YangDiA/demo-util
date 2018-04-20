5f1a7e2e24f41afa4bcf03baae5bf31254ee04b4
Linrry.yang <linrry.yang@shoptps.com>
2018-04-20 10:39:09 +0800

    init

---
+++ util-freemarker/src/main/java/com/demo/util/freemarker/FreeMarkerApplication.java
@@ -0,0 +1,18 @@
+package com.demo.util.freemarker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * (描述用途)
+ *
+ * @author Linrry
+ * @date 2018-04-20 上午 10:21
+ */
+@SpringBootApplication
+public class FreeMarkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreeMarkerApplication.class, args);
+    }
+}
